e06617c0016e42d75a80b06769928ff0f72aec5a
AyonC <ayon@github.com>
2023-11-08 11:59:32 +0530

    remove: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Chivo } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/react";
 
 const chivo = Chivo({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={chivo.className}>
-        <Analytics />
         <Navbar />
         {children}
         <Footer />
